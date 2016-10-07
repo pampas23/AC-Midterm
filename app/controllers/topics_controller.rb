c6d69c09bbcaf59ff39b7804a7715d8caf2a49dd
Pampas Chen <pampas23@gmail.com>
2016-10-07 14:29:59 +0800

    index page & index action init

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,18 @@
 class TopicsController < ApplicationController
 	def index
-		
+    	@topics = Topic.all
+
+  		if params[:id]
+			@topic = Topic.find(params[:id])
+			@sub = "Update"
+		else
+			@topic = Topic.new
+			@sub = "new"
+		end
+
+  		sort_by = (params[:order] == 'comments_count') ? 'comments_count desc' : 'created_at'
+ 		@topics = @topics.order(sort_by).page(params[:page]).per(5)
+ 			
+
 	end
 end
