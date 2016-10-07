3e858651e933be0b531bfa3f765396356f1fec4d
Pampas Chen <pampas23@gmail.com>
2016-10-07 14:39:30 +0800

    add new function in index

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -12,7 +12,27 @@ class TopicsController < ApplicationController
 
   		sort_by = (params[:order] == 'comments_count') ? 'comments_count desc' : 'created_at'
  		@topics = @topics.order(sort_by).page(params[:page]).per(5)
- 			
+ 	end
 
+ 	def new
+		@topic=topic.new
 	end
+
+	def create
+		@topic = Topic.new(topic_params)
+		if @topic.save(topic_params)
+			flash[:notice]="topic saved successfully"
+			redirect_to topics_path
+		else
+			flash[:alert]="topic not saved "
+			render "new"
+		end		
+	end
+
+	private
+
+	def topic_params
+	  params.require(:topic).permit(:title,:content)
+	end
+
 end
